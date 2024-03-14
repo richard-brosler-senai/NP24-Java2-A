ef97f6acb63839e2413031ca19ce6c1edc964455
richard-brosler-senai <richard.brosler@sp.senai.br>
2024-03-13 22:05:47 -0300

    Conteúdo da aula do dia 13-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/richard/secao02/ArteAscii1.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.richard.secao02;
+
+public class ArteAscii1 {
+
+	public static void main(String[] args) {
+		System.out.println("  ▓▓▓▓  ");
+		System.out.println(" ▓    ▓ ");
+		System.out.println("▓ ▓  ▓ ▓");
+		System.out.println("▓      ▓");
+		System.out.println("▓ ▓  ▓ ▓");
+		System.out.println("▓  ▓▓  ▓");
+		System.out.println(" ▓    ▓ ");
+		System.out.println("  ▓▓▓▓  ");
+	}
+
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/richard/secao02/ArteAscii2.java
@@ -0,0 +1,18 @@
+package br.com.senaisp.bauru.richard.secao02;
+
+public class ArteAscii2 {
+
+	public static void main(String[] args) {
+		System.out.println("       _,    _   _    ,_\r\n"
+				+ "  .o888P     Y8o8Y     Y888o.\r\n"
+				+ " d88888      88888      88888b\r\n"
+				+ "d888888b_  _d88888b_  _d888888b\r\n"
+				+ "8888888888888888888888888888888\r\n"
+				+ "8888888888888888888888888888888\r\n"
+				+ "YJGS8P\"Y888P\"Y888P\"Y888P\"Y8888P\r\n"
+				+ " Y888   '8'   Y8P   '8'   888Y\r\n"
+				+ "  '8o          V          o8'\r\n"
+				+ "    `                     `");
+	}
+
+}
